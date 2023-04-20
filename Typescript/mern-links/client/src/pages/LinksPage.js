2ccac0b95a341b11d426d4a197938e1fe5985262
Firsova Liubov <firsova.lyub@gmail.com>
2023-04-20 16:36:50 +0300

    added linkslist component

--- Typescript/mern-links/client/src/pages/LinksPage.js
+++ Typescript/mern-links/client/src/pages/LinksPage.js
@@ -1,7 +1,31 @@
+import c from "config";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { LinksList } from "../components/LinksList";
+import { Loader } from "../components/Loader";
+import { AuthContext } from "../context/Auth.Context";
+import { useHttp } from "../hooks/http.hook";
+
 export const LinksPage = () => {
-  return (
-    <div>
-      <h1>Links page</h1>
-    </div>
-  );
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const { links, setLinks } = useState([]);
+
+  const fetchLinks = useCallback(async () => {
+    try {
+      const fetched = await request("/api/link", "GET", null, {
+        Authorization: `Bearer ${token}`,
+      });
+      setLinks(fetched);
+    } catch (error) {}
+  }, [request, setLinks, token]);
+
+  useEffect(() => {
+    fetchLinks();
+  }, [fetchLinks]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return <>{!loading && <LinksList links={links} />}</>;
 };
